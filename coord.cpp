b29ad0252030d7f20d95641749fa2210e42fa276
ThompZon <ThompZon@gmail.com>
2014-11-27 17:43:39 +0100

    simple implementation

---
+++ coord.cpp
@@ -0,0 +1,35 @@
+#include "coord.hpp"
+
+Coord::Coord(int x, int y){
+	col = x;
+	row = y;
+}
+Coord::Coord(Coord const & other){
+	col = other.col;
+	row = other.row;
+}
+Coord::Coord(Coord const *){
+	col = other->col;
+	row = other->row;
+}
+
+int Coord::get_col() const {
+	return col;
+}
+int Coord::get_row() const {
+	return row;
+}
+bool Coord::set_col(const int x) {
+	if(x < 0) { //TODO upper bound
+		return false;
+	}
+	col = x;
+	return true;
+}
+bool Coord::set_row(const int y) {
+	if(y < 0) { //TODO upper bound
+		return false;
+	}
+	row = y;
+	return true;
+}
