d9dcaeb001735018d1857c8486235db8544d9059
Daniel Langesten <daniel.langest@gmail.com>
2014-11-27 16:07:50 +0100

    started skeletoning GUI

---
+++ gui.hpp
@@ -0,0 +1,14 @@
+
+class GUI {
+	private:
+
+	public:
+		bool move_cursor_up();
+		bool move_cursor_down();
+		bool move_cursor_left();
+		bool move_cursor_right();
+
+		bool draw(int, int, char);
+		bool clear();
+
+};
