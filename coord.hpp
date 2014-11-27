4f5307dd69c4c1c500cb580d6a15f4e4a3a5caf9
ThompZon <ThompZon@gmail.com>
2014-11-27 17:43:05 +0100

    removed reference on set

--- coord.hpp
+++ coord.hpp
@@ -1,9 +1,18 @@
+#ifndef temd_coord
+#define temd_coord
+
 class Coord {
 	private:
 		int col, row;
 	public:
+		Coord(int, int);
+		Coord(Coord const &);
+		Coord(Coord const *);
+
 		int get_col() const;
 		int get_row() const;
-		bool set_col(const int &);
-		bool set_row(const int &);
+		bool set_col(const int);
+		bool set_row(const int);
 };
+
+#endif //temd_coord
