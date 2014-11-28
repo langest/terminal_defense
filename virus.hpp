29e239dc9013a6bfe1c0b86606073cb82551f01c
Daniel Langesten <daniel.langest@gmail.com>
2014-11-28 09:09:25 +0100

    added function and comments

--- virus.hpp
+++ virus.hpp
@@ -4,8 +4,12 @@
 class Virus {
 	private:
 		int hp;
+		const int stamina; //Determines the speed of the virus
 		Path path;
-		bool pathfind();	
+		bool pathfind(); //Find a path through them maze wich it can follow
+		bool step(); //Make the monster walk along its path
+	public:
+		bool update(); //Make the mosnster behave
 };
 
 #endif //termd_virus
