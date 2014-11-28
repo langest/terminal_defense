21fd442723c20b42706e4c5b3f3509019ebafcd3
ThompZon <ThompZon@gmail.com>
2014-11-28 12:34:02 +0100

    gfx and blocking matrix help functions

--- tower/tower.cpp
+++ tower/tower.cpp
@@ -4,6 +4,8 @@ Tower::Tower(const Tower & src) {
 	pos = src.pos;
 	blocking = src.blocking;
 	gfx = src.gfx;
+	size_rows = src.size_rows;
+	size_cols = src.size_cols;
 }
 
 Tower::~Tower() {
@@ -14,13 +16,23 @@ Tower& Tower::operator=(const Tower & src) {
 	pos = src.pos;
 	blocking = src.blocking;
 	gfx = src.gfx;
+	size_rows = src.size_rows;
+	size_cols = src.size_cols;
 	return *this;
 }
 
-void Tower::draw(GUI & g) const {
-	g.draw(pos, gfx);
+void Tower::draw(GUI & g) {
+	g.draw_gfx(pos, gfx, size_rows, size_cols);
 }
 
 inline const bool Tower::get_blocking() const {
 	return blocking;
+}
+
+inline const int Tower::get_size_rows() const {
+	return size_rows;
+}
+
+inline const int Tower::get_size_cols() const {
+	return size_cols;
 }
