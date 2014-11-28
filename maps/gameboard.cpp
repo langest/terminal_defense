a64286f7cd860bcc9def8c9b88d452361b12996b
ThompZon <ThompZon@gmail.com>
2014-11-28 16:51:00 +0100

    update and draw towers

--- maps/gameboard.cpp
+++ maps/gameboard.cpp
@@ -1,17 +1,36 @@
 #include "gameboard.hpp"
 
-GameBoard::GameBoard(){
+GameBoard::GameBoard() {
+	money = 9001; //player money
+
 	blocking = int*[size_rows];
-	for (int i = 0; i < size_rows; ++i)
-	{
+	for (int i = 0; i < size_rows; ++i) {
 		blocking[i] = int[size_cols];
 	}
 }
 
-void GameBoard::draw(GUI & g){
-	//g.draw_gfx(pos, blocking, size_rows, size_cols);
+void GameBoard::draw(GUI & g) {
+	//Draw Towers:
+	for (std::unordered_map<Coord, Tower>::iterator i = towers.begin(); i != towers.end(); ++i;) {
+		if(*i.draw(g) == false) {
+			//If tower failed to draw:
+			#ifdef DEBUGGING
+			#include <iostream>
+			std::cout << "Failed to draw Tower: " << *i << std::endl;
+			#endif //DEBUGGING
+		}
+	}
 }
-bool GameBoard::update(){
+
+bool GameBoard::update() {
+	for (std::unordered_map<Coord, Tower>::iterator i = towers.begin(); i != towers.end(); ) {
+		if(*i.update() == false) {
+			//If tower is flagging removal, remove it!
+			i = tower.erase(i);
+		}else{
+			++i;
+		}
+	}
 	return true;
 }
 
@@ -31,6 +50,17 @@ bool GameBoard::location_availible(Coord c) const {
 	return true;
 }
 
-bool GameBoard::place_tower(Tower t) {
+bool GameBoard::build_tower(Coord c, int tower_id) {
+	switch(tower_id){ //TODO define towers somewhere...
+		case 1: 
+			//is it possible to build here?
+			//enough money?
+			//success?
+			towers.add(Wall_1x1(c));
+			break;
+		default:
+			//unidentified tower_id!
+			return false;
+	}
 	return true;
 }
