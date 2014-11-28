ce5f19cc86ededaf5f2eae24a42bd60f6a3e816f
Daniel Langesten <daniel.langest@gmail.com>
2014-11-28 16:04:20 +0100

    restructured where curses is initiated

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include "gui.hpp"
 
 int main() {
-	GUI gui;
+	initscr(); //Starts curses mode
+	cbreak(); //Since we only want to read characters
+	noecho(); //Turns off echo of input
+	keypad(stdscr, TRUE);
 	
+	Game game;
+
+	game.build_phase();
+	
+	endwin(); //End curses mode
 	return 0;
 }
