beca239d49199c3e55c4d411eb4b47143a7cb137
ThompZon <ThompZon@gmail.com>
2014-12-04 14:49:48 +0100

    setup compilable, callable functions

--- virus/path.cpp
+++ virus/path.cpp
@@ -1,4 +1,29 @@
 #include "path.hpp"
 
-Path::Path(Coord s, Coord g) : start(s), goal(g){
+Path::Path(Coord s) : start(s), curr_pos(s){
+}
+
+Path::Path(const Path & src){
+	start = src.start;
+	curr_pos = src.curr_pos;
+	//TODO keep updated
+}
+
+Path::~Path(){
+	//Nothing needed here at the moment
+}
+
+Path& operator=(const Path & src){
+	start = src.start;
+	curr_pos = src.curr_pos;
+	//TODO keep updated
+	return *this;
+}
+
+Coord Path::get_curr_pos(){
+	return curr_pos;
+}
+
+void Path::step(int sta){
+	//TODO
 }
