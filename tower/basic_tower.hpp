255176316418d813fd4cba173032c95704efe872
Daniel Langesten <daniel.langest@gmail.com>
2014-12-19 10:11:09 +0100

    updated include-guard

---
+++ tower/basic_tower.hpp
@@ -0,0 +1,15 @@
+#ifndef termd_basictower_1x1
+#define termb_basictower_1x1
+
+#include "tower.hpp"
+
+class BasicTower_1x1 : public Tower {
+	private:
+
+	protected:
+
+	public:
+
+}
+
+#endif //terd_basictower_1x1
