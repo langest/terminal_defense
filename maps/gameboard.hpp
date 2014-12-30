8261c67732b54a80fcf5df4f9fc6ce2ed5a7e520
ThompZon <ThompZon@gmail.com>
2014-12-30 13:34:41 +0100

    now uses managers

--- maps/gameboard.hpp
+++ maps/gameboard.hpp
@@ -21,14 +21,13 @@
 #include <cstddef>
 #endif //termd_cstddef
 
-#include "../virus/virusmanager.hpp"
 #include "../definitions"
 #include "../gui.hpp"
-#include "../tower/tower.hpp"
-#include "../tower/wall_1x1.hpp"
-#include "../tower/projectiles/projectile.hpp"
 #include "../coord.hpp"
 #include "../virus/virus.hpp"
+#include "../virus/virusmanager.hpp"
+#include "../tower/towermanager.hpp"
+#include "../tower/projectiles/projectilemanager.hpp"
 
 //class Tower; //Forward declaration because of circle reference
 class GameBoard  {
@@ -41,14 +40,14 @@ class GameBoard  {
 		std::map<Coord, int> grid_bld_state;
 		int size_rows;
 		int size_cols;
-		std::map<Coord, Tower*> towers;
-		std::vector<Projectile*> projectiles;
+		TowerManager tman;
+		ProjectileManager pman;
+		VirusManager vman;
 
 		void draw_towers(GUI &);
 		void draw_viruses(GUI &);
 		void draw_projectiles(GUI &);
 
-		VirusManager virus_manager;
 		void update_towers();
 		void update_projectiles();
 
@@ -72,14 +71,6 @@ class GameBoard  {
 		//Getters and Setters
 		const int get_size_rows() const;
 		const int get_size_cols() const;
-		const int get_env_value(Coord) const;
-		const int get_bld_value(Coord) const;
-		bool location_availible(Coord) const;
-
-		const std::map<Coord, Tower*> & get_towers() const;
-		const std::vector<Virus*> & get_viruses() const;
-		const std::vector<Projectile*> & get_projectiles() const;
-		void add_projectile(Projectile*);
 };
 
 #endif //termd_game_board
