8f5fc1300dd241127e619ac7dc09f2390d8b7a67
ThompZon <ThompZon@gmail.com>
2014-12-30 12:43:18 +0100

    includes, constructor and returntypes

--- tower/towermanager.hpp
+++ tower/towermanager.hpp
@@ -1,18 +1,37 @@
 #ifndef termd_tower_manager
 #define termd_tower_manager
 
+#ifndef termd_map
+#define termd_map
+#include <map>
+#endif //termd_map
+
+#ifndef termd_vector
+#define termd_vector
+#include <vector>
+#endif //termd_vector
+
+#include "../gui.hpp"
+#include "../coord.hpp"
+#include "../definitions"
+#include "tower.hpp"
+#include "wall_1x1.hpp"
+#include "projectiles/projectile.hpp"
+#include "../virus/virus.hpp"
+
+
 class TowerManager {
 	private:
 		std::map<Coord, Tower*> towers;
-		std::vector<Virus*>& dead_towers;
-
-		void purge_towers();
-		void purge_dead_towers();
+		std::vector<Virus*>& viruses;
+		std::vector<Projectile*>& projectiles;
 	public:
+		TowerManager(std::vector<Virus*>&, std::vector<Projectile*>&);
 		~TowerManager();
 
-		bool update();
-		void add_virus(Tower*);
+		void update();
+		void draw(GUI &);
+		void add_tower(Coord, int);
 };
 
 #endif
