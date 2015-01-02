e2f7b8831457f79bb8bcd95ef1a180ddf2cbdfe4
Daniel Langesten <daniel.langest@gmail.com>
2015-01-02 12:02:27 +0100

    revorking stuff

--- game.cpp
+++ game.cpp
@@ -9,10 +9,9 @@ bool Game::build_tower(int tower_id) {
 }
 
 void Game::build_phase() {
-	gui.print_intel(std::string("Build Phase"));
+	int ch;
 	gui.draw_board_frame();
 	gui.draw_intel_frame();
-	int ch;
 	while((ch = getch()) != 27 && ch != 'q') {
 		switch (ch) {
 			case KEY_LEFT:
@@ -41,7 +40,6 @@ void Game::build_phase() {
 }
 
 void Game::invasion_phase() {
-	gui.print_intel("Invasion phase");
 	int ch;
 	board.spawn_virus(0);
 	while (board.update()) {
