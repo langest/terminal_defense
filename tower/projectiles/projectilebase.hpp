aaaf901d25e6661f7c3095bf7d8dcef1a0ff8430
ThompZon <ThompZon@gmail.com>
2015-01-16 15:07:28 +0100

    constructors

--- tower/projectiles/projectilebase.hpp
+++ tower/projectiles/projectilebase.hpp
@@ -5,15 +5,30 @@ namespave termd {
 	struct Projectile_base {
 		char gfx;
 		int damage;
+		int type;
+
+		Projectile_base(char gf, int d, int t) :
+		gfx(gf),
+		damage(d),
+		type(t) {}
 	};
 
-	struct homing_projectile_base : projectile_base {
+	struct Homing_projectile_base : public Projectile_base {
 		int speed_increment;
+
+		Homing_projectile_base(char gf, int d, int t, int speed) :
+		Projectile_base(gf, d, t),
+		speed_increment(speed) {}
 	};
 
-	struct direction_projectile_base : projectile_base {
+	struct Direction_projectile_base : public Projectile_base {
 		int col_spd;
 		int row_spd;
+
+		Direction_projectile_base(char gf, int d, int t, int cs, int rs) :
+		Projectile_base(gf, d, t),
+		col_spd(cs),
+		row_spd(rs) {}
 	};
 }
 
