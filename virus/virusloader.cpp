4af6a325c53dac8220e89332878bf649070bfbda
Daniel Langesten <daniel.langest@gmail.com>
2015-01-16 11:59:51 +0100

    fixed path to info file

--- virus/virusloader.cpp
+++ virus/virusloader.cpp
@@ -3,7 +3,7 @@
 namespace termd {
 	VirusLoader::VirusLoader() {
 		std::ifstream loadfile;
-		loadfile.open("virus.info");
+		loadfile.open("virus/virus.info");
 		if(loadfile.is_open()) {
 			int id;
 			int hp;
