ebe647dbfb43162aabc20807341e87bcbe40c387
Daniel Langesten <daniel.langest@gmail.com>
2015-01-16 11:21:59 +0100

    added wave class

---
+++ maps/wave.cpp
@@ -0,0 +1,26 @@
+#include "wave.hpp"
+
+namespace termd {
+
+	Wave::Wave(VirusManager& vm) :
+		virus_loader(),
+		vman(vm) {}
+
+	void Wave::spawn() {
+		int id(1); //TODO read from wave definition instead
+		// something like this ... for (int id : wavedefinition) ...
+		Virus vir;
+		virus_ptr vp;
+		for (int i = 0; i < 5; ++i) {
+			try {
+				vir = virus_loader.get(id);
+			} catch (const std::invalid_argument &) {
+				continue;
+			}
+			virus_ptr v(new Virus(vir));
+			virus_ptr->move_to(Coord(2*i, 2*i);
+			vman.add_virus(v);
+		}
+	}
+
+}
