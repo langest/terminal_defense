fc985bdd1bef5ccd3e115db5ccda412ab32c0934
Daniel Langesten <daniel.langest@gmail.com>
2015-01-16 11:21:35 +0100

    added move function

--- virus/virus.hpp
+++ virus/virus.hpp
@@ -37,6 +37,7 @@ namespace termd {
 
 			virtual bool update(); //Make the monster behave
 			bool draw() const; //Draw the beauty
+			bool move_to(Coord);
 
 			int get_reward() const;
 			Coord get_pos() const;
