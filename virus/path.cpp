9caf1295a3d490a81c17833b38e2e126217576c0
Daniel Langesten <daniel.langest@gmail.com>
2015-01-26 10:55:22 +0100

    started improving the bfs with a jump point search

--- virus/path.cpp
+++ virus/path.cpp
@@ -11,74 +11,190 @@ namespace termd {
 		}
 	}
 
-	//Algorithm used is not optimal for performance. Though it
-	//should not matter since this is a pretty light-weight
-	//game and it is only performed a limited number of times
-	//on a very restricted amount of nodes.
+	//Implementing jump-point search enhanced BFS
 	Path::Path(Coord start, int num_rows, int num_cols, const std::vector<std::vector<bool> > & towers) {
-		if (start.get_row() >= num_rows || start.get_col() >= num_cols ||
-				start.get_row() < 0 || start.get_col() < 0) return; //invalid start, make no path
-		Coord current(start);
-		std::vector<std::vector<std::pair<int, Coord> > > backtrack( num_rows, std::vector<std::pair<int, Coord> >( num_cols, std::pair<int, Coord>(INT_MAX, Coord(-1,-1))));
 
-		std::queue<Coord> queue;
+		auto is_valid_bounds = [&num_rows, &num_cols](int row, int col) {
+			return row >= 0 &&
+				col >= 0 &&
+				row < num_rows &&
+				col < num_cols;
+		};
 
-		queue.push(current);
-
-
-		while (current.get_col() > 0 && !queue.empty()) {
-			current = queue.front();
-			queue.pop();
-			int r = current.get_row();
-			int c = current.get_col();
-
-			auto check_bound = [&num_rows, &num_cols](int row, int col) {
-				return row >= 0 &&
-					col >= 0 &&
-					row < num_rows &&
-					col < num_cols;
-			};
-			auto step = [&](int row, int col, int cost) {
-				if (backtrack[row][col].first > cost && //shorter path
-					!towers[row][col] ) {
-					queue.push(Coord(row, col));
-					backtrack[row][col] = std::pair<int, Coord>(cost, current);
-				}
-			};
-
-			//Up left
-			if (check_bound(r-1, c-1) && !towers[r-1][c] && !towers[r][c-1])
-				step(r-1, c-1, VIRUS_STEPCOSTDIAGONAL + backtrack[r][c].first);
-			//Down left
-			if (check_bound(r+1, c-1) && !towers[r+1][c] && !towers[r][c-1])
-				step(r+1, c-1, VIRUS_STEPCOSTDIAGONAL + backtrack[r][c].first);
-			//Up right
-			if (check_bound(r-1, c+1) && !towers[r-1][c] && !towers[r][c+1])
-				step(r-1,c+1, VIRUS_STEPCOSTDIAGONAL + backtrack[r][c].first);
-			//Down right
-			if (check_bound(r+1, c+1) && !towers[r+1][c] && !towers[r][c+1])
-				step(r+1,c+1, VIRUS_STEPCOSTDIAGONAL + backtrack[r][c].first);
-			//Left
-			if (check_bound(r, c-1))
-				step(r, c-1, VIRUS_STEPCOST + backtrack[r][c].first);
-			//Right
-			if (check_bound(r, c+1))
-				step(r, c+1, VIRUS_STEPCOST + backtrack[r][c].first);
-			//Up
-			if (check_bound(r-1, c))
-				step(r-1,c, VIRUS_STEPCOST + backtrack[r][c].first);
-			//Down
-			if (check_bound(r+1, c))
-				step(r+1,c, VIRUS_STEPCOST + backtrack[r][c].first);
+		//Check so we have a valid start
+		if (!is_valid_bounds(start.get_row(), start.get_col())) {
+			return;
 		}
+
+		enum DIR {up, down, left, right, up_right, up_left, down_right, down_left};
+		struct Node {
+			Coord coord;
+			DIR dir;
+			Node(Coord cc, DIR dd) : coord(cc), dir(dd){}
+			Node(int r, int c , DIR dd) : coord(r, c), dir(dd){}
+		};
+
+		std::queue<Node> queue;
+		std::vector<std::vector<Coord> > backtrack( num_rows, std::vector<Coord>( num_cols, Coord(-1,-1) ) );
+
+		auto step = [&](Coord came_from, int row, int col, DIR dir) {
+			if (backtrack[row][col].get_row() != -1 &&
+					!towers[row][col] ) {
+				queue.push(Node(row, col, dir));
+				backtrack[row][col] = came_from;
+			}
+		};
+		auto step_straight = [&](Coord came_from, int row, int col, DIR dir) {
+			if (backtrack[row][col].get_row() != -1 &&
+					!towers[row][col] ) {
+				queue.push(Node(row, col, dir));
+				backtrack[row][col] = came_from;
+				switch (dir) {
+					case right:
+						{
+							if (row > 0 && towers[row-1][col-1]) {
+								step(Coord(row, col), row-1, col, up);
+								step(Coord(row, col), row-1, col+1, up_right);
+							}
+								step(Coord(row, col), row, col+1, right);
+							if (row < num_rows && towers[row+1][col-1]) {
+								step(Coord(row, col), row+1, col, down);
+								step(Coord(row, col), row+1, col+1, down_right);
+							}
+						}
+					case left:
+						{
+							if (row > 0 && towers[row-1][col+1]) {
+								step(Coord(row, col), row-1, col, up);
+								step(Coord(row, col), row-1, col-1, up_left);
+							}
+								step(Coord(row, col), row, col-1, left);
+							if (row < num_rows && towers[row+1][col+1]) {
+								step(Coord(row, col), row+1, col, down);
+								step(Coord(row, col), row+1, col-1, down_left);
+							}
+							break;
+						}
+					case up:
+						{
+							break;
+						}
+					case down:
+						{
+							break;
+						}
+					case up_left:
+						{
+							break;
+						}
+					case down_left:
+						{
+							break;
+						}
+					case up_right:
+						{
+							break;
+						}
+					case down_right:
+						{
+							break;
+						}
+				}
+			}
+		};
+
+		//Initialize by starting all directions in the queue
+		Coord current(start);
+		int r = current.get_row();
+		int c = current.get_col();
+		if (is_valid_bounds(r-1, c)) {
+			step_straight(current, r-1, c, up);
+		}
+		if (is_valid_bounds(r+1, c)) {
+			step_straight(current, r+1, c, down);
+		}
+		if (is_valid_bounds(r, c-1)) {
+			step_straight(current, r, c-1, left);
+		}
+		if (is_valid_bounds(r, c+1)) {
+			step_straight(current, r, c+1, right);
+		}
+
+		if (is_valid_bounds(r-1, c-1)) {
+			step(current, r-1, c-1, up_left);
+		}
+		if (is_valid_bounds(r+1, c-1)) {
+			step(current, r+1, c, down_left);
+		}
+		if (is_valid_bounds(r-1, c+1)) {
+			step(current, r-1, c+1, up_right);
+		}
+		if (is_valid_bounds(r+1, c+1)) {
+			step(current, r+1, c+1, down_right);
+		}
+
+		DIR d;
+		while (current.get_col() > 0 && !queue.empty()) {
+			current = queue.front().coord;
+			d = queue.front().dir;
+			r = current.get_row();
+			c = current.get_col();
+			queue.pop();
+
+			switch (d) {
+				case up:
+					{
+						if (is_valid_bounds(r-1, c)) {
+							step(current, r-1, c, up);
+						}
+						break;
+					}
+				case down:
+					{
+						if (is_valid_bounds(r+1, c)) {
+							step(current, r+1, c, down);
+						}
+						break;
+					}
+				case left:
+					{
+						if (is_valid_bounds(r, c-1)) {
+							step(current, r, c-1, left);
+						}
+						break;
+					}
+				case right:
+					{
+						if (is_valid_bounds(r, c+1)) {
+							step(current, r, c+1, right);
+						}
+						break;
+					}
+				case up_left:
+					{
+						break;
+					}
+				case up_right:
+					{
+						break;
+					}
+				case down_left:
+					{
+						break;
+					}
+				case down_right:
+					{
+						break;
+					}
+			}
+		} //End while pathfinding
 
 		std::stack<Coord> reverse;
 		while (current != start) {
 			reverse.push(current);
-			current = backtrack[current.get_row()][current.get_col()].second;
+			current = backtrack[current.get_row()][current.get_col()];
 		}
 		reverse.push(start);
-
 		int cost;
 		Coord last(current);
 		while (!reverse.empty()) {
@@ -93,6 +209,7 @@ namespace termd {
 			path.push(Step(current, cost)); //Copy that 's' Coord!
 			last = current;
 		}
+
 	}
 
 	Path::Path(const Path & src) : path(src.path) {}
