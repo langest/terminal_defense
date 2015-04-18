a5ebc9e677ec9e9326c71fedd8dbef3ff59748b1
Daniel Langesten <daniel.langest@gmail.com>
2015-04-18 16:31:35 +0200

    added functions for getting wave_info

--- maps/waveloader.cpp
+++ maps/waveloader.cpp
@@ -1,1 +1,13 @@
 #include "waveloader.hpp"
+
+Wave_loader::Wave_loader(std::string info_loction) : current_wave(0) {
+	
+}
+
+Wave_loader::has_next() const {
+	return current_wave < loaded_waves.size();
+}
+
+const Wave_info& Wave_loader::get_next_wave() const {
+	return loaded_waves[current_wave++];
+}
--- maps/waveloader.hpp
+++ maps/waveloader.hpp
@@ -2,15 +2,18 @@
 #define termd_wave_loader
 
 #include <vector>
+#include <string>
 
 namespace termd {
 	class Wave_loader {
 		private:
+			int current_wave;
 			std::vector<Wave_info> loaded_waves;
 		public:
-			Wave_loader();
+			Wave_loader(std::string, info_location);
 
-			Wave_info* get_wave(int) const;
+			bool has_next() const;
+			const Wave_info& get_next_wave() const;
 	};
 }
 
