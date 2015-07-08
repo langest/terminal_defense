e9f5ed3c9d6e180f5cfd6e763113e6ad52e770fe
Daniel Langesten <daniel.langest@gmail.com>
2015-07-08 17:11:16 +0200

    Added function calls to the menu items

--- menu.cpp
+++ menu.cpp
@@ -3,18 +3,20 @@
 namespace termd {
 
 	Menu::Menu(Player& p) : player(p) {
-		menu_items.push_back(std::string("Campain"));
-		menu_items.push_back(std::string("Random Level"));
-		menu_items.push_back(std::string("Level Select"));
-		menu_items.push_back(std::string("Load Quicksave"));
-		menu_items.push_back(std::string("Exit"));
+		menu_items.push_back(std::string("Campain")); // item 0
+		menu_items.push_back(std::string("Random Level")); // item 1
+		menu_items.push_back(std::string("Level Select")); // item 2
+		menu_items.push_back(std::string("Load Quicksave")); // item 3
+		menu_items.push_back(std::string("Exit")); // item 4
 
 		std::function<bool()> f = [this]() { return run_game(); };
-		inputcalls['1'] = f;
+		inputcalls[0] = f;
+		inputcalls[1] = f;
+		inputcalls[2] = f;
 		f = [this]() {
 			return load_quicksave(); //TODO Test if valid save-file
 		};
-		inputcalls['2'] = f;
+		inputcalls[3] = f;
 	}
 
 	void Menu::intro() {
@@ -47,23 +49,24 @@ namespace termd {
 	void Menu::run() {
 		intro();
 
-		//TODO restructure here
 		std::vector<Coord> item_pos = print_menu();
 		int ch;
 		int current_item = 0;
 		GUI::move_cursor(item_pos[current_item]);
 		GUI::refresh();
-		while((ch = GUI::get_input()) != '3') {
+		while((ch = GUI::get_input()) != 'q') {
 			if (ch == 'j' || ch == KEY_DOWN) {
 				current_item = (current_item + 1) % item_pos.size();
 			}
-			if (ch == 'k' || ch == KEY_UP) {
+			else if (ch == 'k' || ch == KEY_UP) {
 				--current_item;
 				if (current_item < 0) {
 					current_item = item_pos.size()-1;
 				}
-			} else if (inputcalls.find(ch) != inputcalls.end()) {
-				inputcalls[ch]();
+			} else if ((ch == KEY_ENTER || ch == KEY_RIGHT || ch == 'l') && inputcalls.find(current_item) != inputcalls.end()) {
+				inputcalls[current_item]();
+			} else if ((ch == KEY_ENTER || ch == KEY_RIGHT || ch == 'l') && inputcalls.find(current_item) == inputcalls.end()) {
+				break;
 			}
 			GUI::move_cursor(item_pos[current_item]);
 		}
